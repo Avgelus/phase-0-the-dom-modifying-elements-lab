48dbe5118c38bbc8609cac30a2e4b7fcaf245183
+ <+>
2023-09-02 03:17:15 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.className = "victory";
+newHeader.id = "victory";
+newHeader.textContent = "ANGELUS is the champion";
 // Write your code here!
